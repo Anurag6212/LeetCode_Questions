9f79b3915f0e15c291a8876f822861298fb16f99
Anurag Upadhyay <uanurag6212@gmail.com>
2021-06-12 23:11:41 +0530

    Create  Find_the_Town_Judge.cpp
    Example
    Input: n = 2, trust = [[1,2]]
    Output: 2
    Input: n = 3, trust = [[1,3],[2,3]]
    Output: 3
    Input: n = 3, trust = [[1,3],[2,3],[3,1]]
    Output: -1
    Input: n = 3, trust = [[1,2],[2,3]]
    Output: -1

---
+++ Find_the_Town_Judge.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findJudge(int N, vector<vector<int>>& trust) 
+    {
+            if(N==1)return 1;
+            vector<int> in(N+1),out(N+1);
+            for(auto a:trust )
+            {
+                    in[a[1]]++;
+                    out[a[0]]++;
+                    
+            }
+            for(int i=1;i<=N;i++)
+            {
+                    if(in[i]==N-1 and out[i]==0)return i;
+            }
+            return -1;
+    }
+};
