b77eafcfee3adcba37921be47477cc3bb6b89c68
Anurag Upadhyay <uanurag6212@gmail.com>
2021-06-14 23:28:41 +0530

    Create Slowestkey.cpp

---
+++ Slowestkey.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    char slowestKey(vector<int>& v, string s) {
+        char ans=s[0];
+        int t=v[0];
+        for(int i=0;i<s.size();i++){
+                int d= v[i]-v[i-1];
+                if(d==t){
+                        ans=max(ans,s[i]);
+                }
+                if(d>t){
+                       ans=s[i]; 
+                        t=d;
+                }
+        }
+            return ans;
+    }
+};
