dbb04bcd9ee5951e4b823b26656e243a055047d4
Anurag Upadhyay <uanurag6212@gmail.com>
2021-06-14 23:31:01 +0530

    Update Slowestkey.cpp

--- Slowestkey.cpp
+++ Slowestkey.cpp
@@ -3,7 +3,7 @@ public:
     char slowestKey(vector<int>& v, string s) {
         char ans=s[0];
         int t=v[0];
-        for(int i=0;i<s.size();i++){
+        for(int i=1;i<s.size();i++){
                 int d= v[i]-v[i-1];
                 if(d==t){
                         ans=max(ans,s[i]);
