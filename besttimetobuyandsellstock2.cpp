639372e54700a1ae481379bf3c6a842aeed453ec
Anurag Upadhyay <uanurag6212@gmail.com>
2021-06-19 22:05:03 +0530

    Create besttimetobuyandsellstock2.cpp
    problem link = https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/submissions/

---
+++ besttimetobuyandsellstock2.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) 
+    {
+            int netprofit=0;
+            for(int i=0;i<prices.size();i++)if(prices[i]>prices[i-1])netprofit+=(prices[i]- prices[i-1]);
+            return netprofit;
+    }
+};
