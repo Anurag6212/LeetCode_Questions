371f585d4ed84a5aa244d5f9b5e69ec8fe7e93fe
Anurag Upadhyay <uanurag6212@gmail.com>
2021-08-28 19:59:15 +0530

    Create matrixTranspose.cpp
    problem link =  https://leetcode.com/problems/transpose-matrix/submissions/

---
+++ matrixTranspose.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) 
+    {
+            int n=matrix.size();
+            int m=matrix[0].size();
+            vector<vector<int>>mat(m,vector<int>(n,0));
+            for(int j=0;j<m;j++)
+            {
+                    for(int i=0;i<n;i++)
+                    {
+                                  mat[j][i]=matrix[i][j];
+                    }
+            }
+            return mat;
+    }
+};
