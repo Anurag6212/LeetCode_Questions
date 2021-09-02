c72ff6e2c4d252a7d04901be804ad84e42f0c74d
Anurag Upadhyay <uanurag6212@gmail.com>
2021-09-02 21:07:31 +0530

    Create double_exist.cpp
    problem link = https://leetcode.com/problems/check-if-n-and-its-double-exist/

---
+++ double_exist.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) 
+    {
+            for(int i=0;i<arr.size();i++)
+            {
+                    for(int j=arr.size()-1;j>=0;j--)
+                    {
+                            if(i!=j && arr[i]==2*arr[j])
+                            {
+                                    return true;
+                            }
+                    }
+            }
+            return false;
+    }
+};
