5728733f18fe53f46463b32a589a9d45fc7e1125
Anurag Upadhyay <uanurag6212@gmail.com>
2021-09-07 17:04:58 +0530

    Create maxVal.cpp
    Problem link - https://leetcode.com/problems/maximum-value-at-a-given-index-in-a-bounded-array/

---
+++ maxVal.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int check(int n,int i,int maxSum,int m)
+    {
+            for (int d = 1; maxSum >= 0 && d < m && i + d < n; ++d)
+                    maxSum -= m - d;
+            for (int d = 1; maxSum >= 0 && d < m && i - d >= 0; ++d)
+                    maxSum -= m - d;
+            return maxSum >= 0;
+    }
+    int maxValue(int n, int index, int maxSum) 
+    {
+            int l=0;
+            int r=maxSum;
+            while(l<r)
+            {
+                    int m=(l+r+1)/2;
+                    if(check(n,index,maxSum-n-m,m))
+                    {
+                            l=m;
+                    }
+                    else 
+                    {
+                            r=m-1;
+                    }
+            }
+            return l+1;
+    }
+};
